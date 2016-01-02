e2deaea1c67189e05ab93f60471279fa41d5af98
Soumya Ray <soumya.ray@gmail.com>
2016-01-02 12:21:18 +0800

    Allow fake queue to be used for dev/testing

--- services/ChoiceSetService.rb
+++ services/ChoiceSetService.rb
@@ -1,4 +1,5 @@
 require 'aws-sdk'
+require 'ostruct'
 
 class ChoiceSet
   attr_reader :replication
@@ -27,9 +28,13 @@ end
 class ChoiceSetService
   TIMEOUT_MINUTES = 20
 
-  def initialize
-    @sqs ||= Aws::SQS::Client.new
-    @q_url ||= @sqs.get_queue_url(queue_name: 'AppStoreQueue').queue_url
+  def initialize(fake: false)
+    # Use a fake choiceset queue if requested, or AWS credentials not present
+    if (!fake && ENV.has_key?('AWS_REGION'))
+      @queue = ChoiceSetQueue.new
+    else
+      @queue = FakeSetQueue.new
+    end
   end
 
   ##
@@ -37,7 +42,7 @@ class ChoiceSetService
   # Returns: ChoiceSet object if valid choiceset still exists
   #          nil if no more choicesets found (NOTE: could be temporary)
   def start_choiceset
-    msg = @sqs.receive_message(queue_url: @q_url).messages.first
+    msg = @queue.fetch
     rep, config = msg.body.split(':').map(&:to_i)
     set = ChoiceSet.new(rep, config, msg.receipt_handle)
   end
@@ -47,7 +52,22 @@ class ChoiceSetService
   # Parameters: ChoiceSet object
   # Returns: true if success, Exception
   def close_choiceset(receipt)
-    resp =  @sqs.delete_message({queue_url: @q_url, receipt_handle: receipt})
+    @queue.done(receipt)
+  end
+end
+
+class ChoiceSetQueue
+  def initialize
+    @sqs = Aws::SQS::Client.new
+    @q_url = @sqs.get_queue_url(queue_name: 'AppStoreQueue').queue_url
+  end
+
+  def fetch
+    msg = @sqs.receive_message(queue_url: @q_url).messages.first
+  end
+
+  def done(receipt)
+    @sqs.delete_message({queue_url: @q_url, receipt_handle: receipt})
     true
   rescue Aws::SQS::Errors::ReceiptHandleIsInvalid
     raise ArgumentError, "Input receipt \"#{receipt}\" is not a valid receipt"
@@ -55,12 +75,12 @@ class ChoiceSetService
     raise RuntimeError, "Could not send the delete request to SQS", e
   end
 
-  private
-
-  def fill_queue()
-    reps = (1..3)
-    combos = (1..144)
-
+  ##
+  # Fill the queue wil repititions and combinations
+  # Parameters: reps and combos, each as array or range
+  # Ex.:  fill_queue(1..3, 1..144)
+  #       fill_queue([1,3], 1..144)
+  def fill_queue(reps, combos)
     reps.each do |r|
       combos.each do |c|
         msg = "#{r}:#{c}"
@@ -68,6 +88,57 @@ class ChoiceSetService
         @sqs.send_message(queue_url: @q_url, message_body: msg)
       end
     end
+    self
+  end
+
+  def sets_remaining
+    @sqs.get_queue_attributes(
+      queue_url: @q_url,
+      attribute_names: ['ApproximateNumberOfMessages']
+    ).attributes['ApproximateNumberOfMessages'].to_i
+  end
+end
+
+class FakeSetQueue
+  @@queue = Queue.new
+  @@dead_queue = Array.new
+
+  def initialize
+    fill_queue(1..3, 1..144) if @@queue.size == 0
+  end
+
+  def fetch
+    msg_body = @@queue.pop
+    @@dead_queue.push(msg_body)
+    OpenStruct.new(receipt_handle: msg_body, body: msg_body)
+  end
+
+  def done(receipt)
+    if @@dead_queue.delete(receipt)
+      true
+    else
+      raise ArgumentError, "Input receipt \"#{receipt}\" is not a valid receipt"
+    end
+  end
+
+  ## CAUTION: DO NOT USE THIS METHOD UNLESS YOU WANT TO FILL THE QUEUE
+  # Fill the queue wil repititions and combinations
+  # Parameters: reps and combos, each as array or range
+  # Ex.:  fill_queue(1..3, 1..144)
+  #       fill_queue([1,3], 1..144)
+  def fill_queue(reps, combos)
+    reps.each do |r|
+      combos.each do |c|
+        msg = "#{r}:#{c}"
+        print "#{msg}, "
+        @@queue.push(msg)
+      end
+    end
+    self
+  end
+
+  def sets_remaining
+    @@queue.size
   end
 end
 
