4a7cc90943b1a1ab9d45d583d59ea37ea6c52750
ChenShirley <ChenShirley@users.noreply.github.com>
2016-01-02 17:33:50 +0800

    using ChoiceSetService in controller

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,5 @@ module AppStore
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 		config.assets.initialize_on_precompile = false
-		config.autoload_paths += %W("#{config.root}/app")
   end
 end
