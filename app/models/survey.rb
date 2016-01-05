80dd58e066e4c0d2f175fbc9d3f5ad3b1bd276a9
ChenShirley <ChenShirley@users.noreply.github.com>
2016-01-05 20:45:33 +0800

    remove attention check, and adjust suggestion to comment

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,6 +1,6 @@
 class Survey < ActiveRecord::Base
 	validates_presence_of :age, :gender, :income, :involvement1, :involvement2, :involvement3, :rf1, :rf2, :rf3, :rf4, :rf5, :rf6, :rf7,
-												:why, :attention
+												:why
   validates :age, numericality: { only_integer: true }
   attr_accessible :esearch, :regulatory_focus,
 									:age, :gender, :income,
